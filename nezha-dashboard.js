3a51272bff16eae6fa60a6e5a46f19a74b653399
Squarelan <46771456+Squarelan@users.noreply.github.com>
2025-12-26 08:37:12 +0800

    Update neZhaConfig properties for admin settings

--- nezha-dashboard.js
+++ nezha-dashboard.js
@@ -3,8 +3,8 @@
 
 // ===== 配置区域 =====
 const neZhaConfig = {
-    disableAnimatedMan: true,        // 是否禁用动画
-    adminTitle: 'v2.games',          // 左上角后台名称
+    disableAnimatedMan: 'true',        // 是否禁用动画
+    adminTitle: 'Squarelan',          // 左上角后台名称
     fontUrl: 'https://font.sec.miui.com/font/css?family=MiSans:400,700:MiSans', // 自定义字体
     fontFamily: 'MiSans',            // 字体名称
 };
